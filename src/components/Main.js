060772a372f878fb48861c7fda236b436284e9a4
Behruz Rajabov <behruzgeniusavenue@gmail.com>
2024-08-12 07:44:31 +0500

    modified:   src/components/Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import AuctionSection from './AuctionSection';
 import Creators from './CreatorsSection'
 const Main = () => {  
       return (
-        <div className="App">
+        <div className="App nothing">
           <AuctionSection title="Live Auctions🔥" id="live-auctions" auctionsFile="liveAuction" />
           <AuctionSection title="Featured" id="featured-auctions" auctionsFile="featuredAuction" initialVisibleCount='8' />
           <Creators />
